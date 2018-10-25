3310ac28f117589fb82e18dcfc4477df6212769d
BryanPlaces <eduardo.places@gmail.com>
2018-10-25 14:04:24 +0100

    Creacion y uso de controladores

--- routes/web.php
+++ routes/web.php
@@ -15,26 +15,14 @@ Route::get('/', function () {
     return 'Home';
 });
 
-Route::get('/usuarios', function () {
-    return 'Usuarios';
-});
-
-Route::get ('usuarios/{id}', function($id) {
-    return "Mostrando detalle del usuario: {$id}";
-})->where ("id", "[0-9]+");
-
-Route::get ('usuarios/nuevo', function () {
-    return "Crear nuevo usuario";
-});
+Route::get('/usuarios', 'UserController@index');
 
 
-Route::get('saludo/{name}/{nickname?}', function ($name, $nickname = null) {
+Route::get ('usuarios/{id}', 'UserController@show')
+    ->where ("id", "[0-9]+");
 
-    $name = ucfirst($name);
 
-    if ($nickname) {
-        return "Bienvenido {$name}, tu apodo es {$nickname}";
-    }else {
-        return "Bienvenido {$name}";
-    }
-});
+Route::get ('usuarios/nuevo', 'UserController@create');
+
+
+Route::get('saludo/{name}/{nickname?}', 'WelcomeUserController@index');
